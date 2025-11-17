c5dffdccc7ddab3d61f9b53b8de1e1d9d37d9931
doduyquy <doduyquy211@gmail.com>
2025-11-17 08:53:52 +0700

    Quy - admin: reindexed sequence and add optimize route cache for quickly response

--- ssb-optimizer_python/app.py
+++ ssb-optimizer_python/app.py
@@ -9,12 +9,53 @@ PYTHON_SERVICE_PORT = 5111
 SGU_LAT = 10.759983082120561
 SGU_LNG = 106.68225725256899
 
-
 app = Flask(__name__)
 
-# allow POST method
+# Save optimized route for response quickly, cache it in memory
+optimized_routes_cache = {}
+
+def reIndexSequenceAfterOptimize(optimized_routes):
+    """
+    Re-index the sequence of bus stops after optimization.
+    Input:
+        optimized_routes: {
+            "cluster_1": [ {lat, lng, sequence}, ... ],
+            "cluster_2": [ {lat, lng, sequence}, ... ],
+            ...
+        }
+    Output:
+        optimized_routes with updated sequence starting from 1 
+        (structure like the input but only sequence is reindexed for each cluster)
+    """
+    reindexed_routes = {}
+    for cluster_id, stops in optimized_routes.items():
+        reindexed_stops = []
+        for index, stop in enumerate(stops):
+            stop["sequence"] = index + 1
+            reindexed_stops.append(stop)
+        
+        reindexed_routes[cluster_id] = reindexed_stops
+
+    return reindexed_routes
+
 @app.route("/optimize", methods=["POST"])
-def optimize():
+# ------------------OPTIMIZE ROUTE-------------------
+def optimize(isReOptimize=False):
+    """Optimize route endpoint
+    Input: JSON data from NodeJS with list of bus stops
+    Output: JSON data with optimized routes for each cluster (including re-index for sequence column)
+    """
+
+    # Nếu có cache và isReOptimize=False thì trả về cache
+    # trong trường hợp load lại trang hay back lại trang từ browser, không cần tối ưu lại
+    global optimized_routes_cache
+    if not isReOptimize and optimized_routes_cache:
+        print("--> Trả về kết quả đã cache")
+        return jsonify({
+            "success": True,
+            "optimizedRoutes": optimized_routes_cache
+        }), 200
+    # Nếu không có cache hoặc isReOptimize=True thì chạy lại từ đầu
     try:
         # 1. Get data(list lat/lng) from NodeJS
         data = request.get_json()   # get JSON from NodeJS
@@ -31,20 +72,27 @@ def optimize():
         clustering_result = cluster_busStop(data)
         print(f"\n\n->_<- Clustering result:\n\n", clustering_result["clusters"])
 
-
-        # ----------------------------------------------------
+        # -------------------Optimize------------------
         sgu_location = {"lat": SGU_LAT, "lng": SGU_LNG}
         clusters = clustering_result["clusters"]
         # Optimize
         optimized_routes = solve_tsp_for_clusters(clusters=clusters, sgu_location=sgu_location)
+        
+        # ------------------Reindexed sequence---------
+        optimized_routes_after_reindexed_sequence = reIndexSequenceAfterOptimize(optimized_routes)
         # ----------------------------------------------------
 
+        # ------------------Update cache---------------
+        optimized_routes_cache = optimized_routes_after_reindexed_sequence  # Update cache
+        print("--> Đã cập nhật cache kết quả tối ưu.")
+        # ----------------------------------------------------
+        
         print(f"\n\n->_<- Route result after optimized:\n\n", optimized_routes)
         
         # 3. Return JSON data to NodeJS with status code 200 (OK)
         return jsonify({
             "success": True,
-            "optimizedRoutes": optimized_routes
+            "optimizedRoutes": optimized_routes_after_reindexed_sequence
         }), 200
         
     except Exception as e:
@@ -54,6 +102,7 @@ def optimize():
             "success": False,
             "error": f"Lỗi server: {str(e)}"
         }), 500
+# -----------------------=|=--------------------------
 
 # Health check endpoint: kiểm tra xem api có hoạt động oke ko, trước khi run phần chính
 @app.route("/health", methods=["GET"])
@@ -73,3 +122,6 @@ def health():
 
 if __name__ == "__main__":
     app.run(port=PYTHON_SERVICE_PORT)
+
+
+
